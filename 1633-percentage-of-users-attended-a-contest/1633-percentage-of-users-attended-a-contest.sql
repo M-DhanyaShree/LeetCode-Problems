8f122c4ea4566b25fccf6d49f8caa43624aad851
Dhanya Shree M <dhanyashree140@gmail.com>
2026-05-06 22:18:16 +0530

    Time: 1136 ms (16.85%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select contest_id, ROUND(((COUNT(user_id)*100.0)/(select count(*) from users)),2) as percentage from Register group by contest_id order by percentage DESC, contest_id ASC;
