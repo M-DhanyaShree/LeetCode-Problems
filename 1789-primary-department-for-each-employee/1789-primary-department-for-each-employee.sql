1f3de1b13f229515829be6786c665e0987563a8b
Dhanya Shree M <dhanyashree140@gmail.com>
2026-05-16 00:01:51 +0530

    Time: 578 ms (34.54%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select employee_id, department_id from Employee
+where primary_flag='Y' or (primary_flag='N' and employee_id not in (select employee_id from Employee where primary_flag='Y'));
