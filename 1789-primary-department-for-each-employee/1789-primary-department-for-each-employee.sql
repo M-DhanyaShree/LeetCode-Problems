13124fa3e6766f0d95d6c9f17371a230e971eaf1
Dhanya Shree M <dhanyashree140@gmail.com>
2026-05-16 00:04:18 +0530

    Time: 568 ms (41.35%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select employee_id, department_id from Employee
-where primary_flag='Y' or (primary_flag='N' and employee_id not in (select employee_id from Employee where primary_flag='Y'));
+where primary_flag='Y' or employee_id in (select employee_id from Employee group by employee_id having count(*)=1);
