c49c3988a6545dd50683a66f2e48eea0c51c8003
Dhanya Shree M <dhanyashree140@gmail.com>
2026-05-17 23:39:24 +0530

    Time: 1220 ms (39.55%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+select top 1 person_name from (
+    select person_name,
+    sum(weight) over(order by turn) as total_weight from Queue
+)a where total_weight<=1000 order by total_weight desc;
